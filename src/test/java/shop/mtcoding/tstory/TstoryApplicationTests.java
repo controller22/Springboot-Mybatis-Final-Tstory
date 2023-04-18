4a1be64c9d07d5daa15dba1856422163c1b49ee1
DH <shim5013@naver.com>
2023-04-18 18:29:55 +0900

    초기세팅완료

---
+++ src/test/java/shop/mtcoding/tstory/TstoryApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.tstory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TstoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
