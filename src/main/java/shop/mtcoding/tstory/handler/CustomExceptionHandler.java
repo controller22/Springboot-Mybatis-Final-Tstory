3158153e322e127b08f0db29cdd67e6a2f874f84
DH <shim5013@naver.com>
2023-04-21 10:22:30 +0900

    초기세팅 수정 완료

---
+++ src/main/java/shop/mtcoding/tstory/handler/CustomExceptionHandler.java
@@ -0,0 +1,27 @@
+package shop.mtcoding.tstory.handler;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import shop.mtcoding.tstory.dto.ResponseDto;
+import shop.mtcoding.tstory.handler.ex.CustomApiException;
+import shop.mtcoding.tstory.handler.ex.CustomException;
+import shop.mtcoding.tstory.util.Script;
+
+@RestControllerAdvice
+public class CustomExceptionHandler {
+
+    // NullPointException <- RuntimeException
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<?> customException(CustomException e) {
+        String responseBody = Script.back(e.getMessage());
+        return new ResponseEntity<>(responseBody, e.getStatus());
+    }
+
+    @ExceptionHandler(CustomApiException.class)
+    public ResponseEntity<?> customApiException(CustomApiException e) {
+        return new ResponseEntity<>(new ResponseDto<>(-1, e.getMessage(), null), e.getStatus());
+    }
+}
+
